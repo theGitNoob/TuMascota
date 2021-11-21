bbd4b3fa3aaaf3ed19b7029b0a25f77dd954a815
theGitNoob <racosta011220@gmail.com>
2021-11-21 14:56:51 -0500

    modificado nodemailer para usar gmail

--- config/nodemailer-config.js
+++ config/nodemailer-config.js
@@ -1,16 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.nauta.cu",
-  port: 25,
-  secure: false, // upgrade later with STARTTLS
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
-  },
-  tls: {
-    // do not fail on invalid certs
-    rejectUnauthorized: false,
   },
 });
 
